442915bd1ebd266fcdc785462b4c96c5c1ae79bc
krasny-hohot <156503165+krasny-hohot@users.noreply.github.com>
2024-01-13 10:51:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,15 @@ import App from './App.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import { createVuetify } from 'vuetify';
-import firebase from 'firebase/app'; // Import only the Firebase App module
-
-import 'firebase/database'; // Import the Firebase Realtime Database module (or other Firebase modules if needed)
-
+import firebase from 'firebase/app'; 
+import 'firebase/database';
 Vue.config.productionTip = false;
-
 Vue.use(Vuetify);
-
 const vuetify = createVuetify({
   theme: {
     defaultTheme: 'light',
-    // Additional theme configurations go here
   },
 });
-
 new Vue({
   render: (h) => h(App),
   vuetify,
